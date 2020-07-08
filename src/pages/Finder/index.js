f4043a884bbf0a4db63cb786f44825649bee912c
joaoptgaino <joaoptgaino@hotmail.com>
2020-07-08 09:57:31 -0300

    Changes in 'finding lyrics' screen

--- src/pages/Finder/index.js
+++ src/pages/Finder/index.js
@@ -1,29 +1,72 @@
 import { StatusBar } from 'expo-status-bar';
 import React from 'react';
-import { StyleSheet, Text, View, KeyboardAvoidingView, Platform } from 'react-native';
-
+import Constants from "expo-constants";
+import { Feather as Icon } from '@expo/vector-icons';
+import axios from 'axios';
+import { StyleSheet, TouchableOpacity, Text, View, KeyboardAvoidingView, Platform, TextInput,Button} from 'react-native';
+import { useNavigation } from '@react-navigation/native';
 export default function Finder() {
+    const navigation = useNavigation()
+    function findLyrics(title, artist) {
+        return axios.get(`https://api.lyrics.ovh/v1/${artist}/${title}`)
+    }
+    function navigateToHome() {
+        navigation.navigate('Home');
+    }
+    function handleInputChange() {
+        console.log("Hello");
+    }
+    function handleSubmit(){
+        console.log("Button");
+    }
     return (
         <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
             <View style={styles.container}>
+                <TouchableOpacity onPress={navigateToHome}>
+                    <Icon name="arrow-left" size={20} color="black" />
+                </TouchableOpacity>
+                <Text style={styles.title}>Finder screen</Text>
                 <View style={styles.main}>
-                    <Text style={styles.title}>Finder screen</Text>
+                    <Text>Artist name</Text>
+                    <TextInput
+                        style={styles.formControl}
+                        onChange={handleInputChange}
+                        placeholder="Megadeth"
+                    />
+                    <Text>Song name</Text>
+                    <TextInput
+                        style={styles.formControl}
+                        onChange={handleInputChange}
+                        placeholder="She-wolf"
+                    />
+                    <Button style={styles.submitButton} title="Find out" onPress={handleSubmit}/>
                 </View>
             </View>
         </KeyboardAvoidingView>
-  );
+    );
 }
 
 const styles = StyleSheet.create({
-    container:{
-        flex:1,
-        padding:32,
+    container: {
+        flex: 1,
+        paddingHorizontal: 32,
+        paddingTop: 20 + Constants.statusBarHeight,
     },
-    main:{
-        flex:1,
-        justifyContent:'center',
+    main: {
+        flex: 1,
+        justifyContent: 'center',
     },
-    title:{
-        marginTop:50,
+    title: {
+        marginTop: 50,
+        justifyContent: 'center',
+        textAlign: 'center',
+        fontSize: 24
+    },
+    formControl: {
+        padding: 2,
+        color:'darkgray',
+    },
+    submitButton:{
+        backgroundColor:'red',
     }
 })
--- src/pages/Home/index.js
+++ src/pages/Home/index.js
@@ -11,14 +11,16 @@ export default function Home() {
     }
     return (
         <KeyboardAvoidingView style={{ flex: 1 }} behavior={Platform.OS === 'ios' ? 'padding' : undefined}>
+
             <View style={styles.container}>
+                <Text style={styles.title}>Lyrics Finder</Text>
                 <View style={styles.main}>
-                    <Text style={styles.title}>Lyrics Finder</Text>
-                    <Text style={styles.description}>Lyrics Finder</Text>
+
+                    <Text style={styles.description}>Let's find some lyrics for your songs?</Text>
 
                     <RectButton style={styles.button} onPress={handleNavigateToFinder}>
                         <Text style={styles.buttonText}>
-                            Entrar
+                            Let's go!
                     </Text>
                     </RectButton>
                 </View>
@@ -35,34 +37,39 @@ const styles = StyleSheet.create({
     main: {
         flex: 1,
         justifyContent: 'center',
+        textAlign: 'center',
     },
     title: {
+        padding:10,
         color: 'black',
         fontSize: 32,
-        maxWidth: 260,
-        maxHeight: 64
+        maxWidth: 360,
+        maxHeight: 64,
+        textAlign: 'center',
     },
     description: {
         color: 'black',
-        fontSize: 16,
+        fontSize: 20,
         marginTop: 16,
-        maxWidth: 260,
-        lineHeight: 24
-    },
-    button:{
-        backgroundColor:'gold',
-        height:60,
-        flexDirection:'row',
-        borderRadius:10,
-        overflow:'hidden',
-        alignItems:'center',
-        marginTop:8,
-    },
-    buttonText:{
-        flex:1,
-        justifyContent:'center',
+        maxWidth: 400,
+        lineHeight: 24,
         textAlign:'center',
-        color:'black',
-        fontSize:16,
+    },
+    button: {
+        backgroundColor: 'gold',
+        height: 60,
+        flexDirection: 'row',
+        borderRadius: 10,
+        overflow: 'hidden',
+        alignItems: 'center',
+        marginTop: 8,
+    },
+    buttonText: {
+        fontWeight:'bold',
+        flex: 1,
+        justifyContent: 'center',
+        textAlign: 'center',
+        color: 'black',
+        fontSize: 16,
     }
 });
